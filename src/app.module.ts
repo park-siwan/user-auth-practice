f74133062221425183ae363e66ef8b953ff297c4
park-siwan <siwandevelop@gmail.com>
2023-09-19 11:26:59 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PodcastsModule } from './podcast/podcasts.module';
+import { Podcast } from './podcast/entities/podcast.entity';
+import { Episode } from './podcast/entities/episode.entity';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite3',
+      synchronize: true,
+      logging: true,
+      entities: [Podcast, Episode],
+    }),
+    GraphQLModule.forRoot({ autoSchemaFile: true, driver: ApolloDriver }),
+    PodcastsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
